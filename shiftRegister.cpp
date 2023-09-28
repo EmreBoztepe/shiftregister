90991f1ab6ac13176278dbcfbae905069eb293c5
EmreBoztepe <61236072+EmreBoztepe@users.noreply.github.com>
2023-09-28 10:54:41 +0300

    for debugging purpose..

--- shiftRegister.cpp
+++ shiftRegister.cpp
@@ -1,11 +1,13 @@
 #include "shifRegister.h"
-
+#include <iostream>
 
 int main ( void )
 {
     shifRegister u101(45,14,12,11);
     shifRegister u102(44,34,24,14);
 
+    std::cout<<"hello"<< std::endl;
+    
     
 
 
